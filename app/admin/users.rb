e05fe2bb2a008fd27a5b2d3e2fdeabd04a199f92
nikiforov5000 <nikiforov5000@gmail.com>
2022-06-13 15:52:10 +0300

    login with phone_no

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register User do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :last_name, :first_name, :email
+  permit_params :last_name, :first_name, :email, :phone_no
   #
   # or
   #
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :email_confirmation, :login
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:phone_no]
          
+
   has_many :appointments
   has_many :doctors, through: :appointments
 end
